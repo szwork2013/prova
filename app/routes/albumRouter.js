18cd89e61c78f03b6371898e2bf7894827a0a46f
Gianpiero Ferraro <gianpie@yahoo.it>
2015-07-29 02:53:01 +0200

    commit

--- app/routes/albumRouter.js
+++ app/routes/albumRouter.js
@@ -1,22 +1,15 @@
-/**
- * Created by Sysdata on 27/07/2015.
- */
-/**
- * Created by Gianpiero on 17/07/2015.
- */
 var express = require('express'),
-    fs = require('fs'),
-    config = require('./../../config/environment'),
-    fs_service = require('../services/fs_service');
+    //fs = require('fs'),
+    config = require('./../../config/environment');
+    //fs_service = require('../../fs_service');
+    //logger = require('../../logger')(config);
 
-var routes = function(Album){
-
-
+var routes = function(Album,fs_service,logger){
     var albumRouter = express.Router();
 
     albumRouter.route('/')
         .get(function(req,res){
-            console.log("getting all albums");
+            logger.info("getting all albums");
             Album.find(function(err,albums){
                 if (!err) {
                     res.json(albums);
@@ -27,15 +20,16 @@ var routes = function(Album){
         })
         .post(function(req,res) {
 
-            console.log("saving one album");
+            logger.info("saving one album");
             var album = new Album(req.body);
             var dir = 'images_uploaded/' +album._id;
 
             var images = req.body.images;
 
             if (images) {
-
                 var fileToSave = images.length;
+                logger.info("saving album with: "+fileToSave+" images");
+
                 var fileSaved = 0;
                 var writeFailed = false;
 
@@ -49,37 +43,42 @@ var routes = function(Album){
 
                     function onSuccessWrite(){
                         fileSaved++;
-                        console.log("OnSuccessWrite: " + fileSaved + " / "+fileToSave);
+                        logger.info("OnSuccessWrite: " + fileSaved + " / "+fileToSave);
 
                         if(fileSaved == fileToSave)
                         {
-                            console.log("saving album into mongodb")
-                            album.save(function(err,album){
-                                if (!err) {
-                                    res.json(album);
-                                } else {
-                                    res.status(500).send(err);
-                                }
-                            });
+                            saveAlbum();
                         }
                     }
 
-                    function onErrorWrite(){
-                        console.log("callback funcition onErrorWrite");
+                    function onErrorWrite(err){
+                        logger.debug("onErrorWrite: " +err);
                         writeFailed = true;
                         res.status(500).send("album not saved");
                     }
 
                 });
-
-
+            }
+            else{
+                logger.info("saving album without images");
+                saveAlbum();
             }
 
+            function saveAlbum(){
+                logger.debug("saving album into mongodb")
+                album.save(function(err,album){
+                    if (!err) {
+                        res.json(album);
+                    } else {
+                        res.status(500).send(err);
+                    }
+                });
+            }
         });
 
         albumRouter.route('/:album_id')
             .get(function(req,res){
-                console.log("getting album: " + req.params.album_id);
+                logger.info("getting album: " + req.params.album_id);
                 Album.findById(req.params.album_id,function(err,album){
                     if (!err) {
                         res.json(album);
@@ -90,15 +89,13 @@ var routes = function(Album){
             })
             .delete(function(req,res){
                 var album_id = req.params.album_id;
-                console.log("deleting album: " + album_id);
-
+                logger.info("deleting album: " + album_id);
                 Album.findById(album_id,function(err,album){
                     if(err)
                         res.status(500).send("album not found");
                     else {
                         var path= config.images_dir  + 'images_uploaded/' +album_id;
                         album.remove();
-
                         fs_service.deleteAlbum(path);
                         res.status(204).send();
                     }
---
+++ services/logger.js
@@ -0,0 +1,25 @@
+var winston = require('winston');
+
+var logger = function(config){
+    return new (winston.Logger)({
+        transports: [
+            new (winston.transports.Console)({ json: true, timestamp: true}),
+            new winston.transports.File({ filename: config.log_dir + '/debug.log', json: false })
+        ],
+        exceptionHandlers: [
+            new (winston.transports.Console)({ json: false, timestamp: true }),
+            new winston.transports.File({ filename: config.log_dir + '/exceptions.log', json: false })
+        ],
+        exitOnError: false,
+
+    });
+
+
+
+}
+
+module.exports = logger;
+
+
+
+
